6f0639ea1562ca0dbae3ec837b0d54ee12cc6b9b
amir el-shennawy <amir@el-shennawy.de>
2023-12-19 16:49:11 +0100

    added _locals, updated aws version to 5.26, used local variables in vpc.tf instead of hardcoded ones.

--- vpc.tf
+++ vpc.tf
@@ -13,7 +13,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc-projectPOC"
-  cidr = "10.0.0.0/16"
+  cidr = local.vpc_cidr
 
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
