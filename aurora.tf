266571bc3eec2017eb5312a8512c94cbccdede12
amir el-shennawy <amir@el-shennawy.de>
2024-01-15 15:38:43 +0100

    changed aurora from engine version 3.x to 2.x to use db.t2.small since they are not compatible with newer engine versions

--- aurora.tf
+++ aurora.tf
@@ -3,7 +3,7 @@ module "cluster" {
 
   name                       = "aurora-db-mysql"
   engine                     = "aurora-mysql"
-  engine_version             = "8.0.mysql_aurora.3.02.0"
+  engine_version             = "5.7.mysql_aurora.2.11.2"
   master_username            = "root"
   auto_minor_version_upgrade = true #
 
